63dc2e648f93c7917cced6b08305255f8e4ce330
octospacc <octo@tutamail.com>
2024-02-24 20:26:34 +0100

    Update SpaccDotWebServer to add all currently needed features, update example

--- Example.Server.js
+++ Example.Server.js
@@ -1,16 +1,20 @@
-const SpaccDotWebServer = require('./SpaccDotWeb.Server.js')({
+const SpaccDotWebServer = require('./SpaccDotWeb.Server.js');
+const server = SpaccDotWebServer.setup({
 	appName: 'Example',
 	// staticPrefix: '/static/',
 	// staticFiles: [],
 	linkStyles: [ 'Example.css' ],
 	// linkScripts: [],
-	// htmlPager: htmlPager(content) => `...`,
+	// pageTitler: pageTitler(title) => `...`,
+	// appPager: appPager(content, title) => `...`,
+	// htmlPager: htmlPager(content, title) => `...`,
 });
 
-if (process && process.argv[2] === 'html') {
-	SpaccDotWebServer.writeStaticHtml(__filename);
+if (SpaccDotWebServer.envIsNode && process.argv[2] === 'html') {
+	server.writeStaticHtml();
+	console.log('Dumped Static HTML!');
 } else {
-	SpaccDotWebServer.initServer({
+	server.initServer({
 		// defaultResponse: { code: 500, headers: {} },
 		// endpointsFalltrough: false,
 		// port: 3000,
@@ -19,7 +23,7 @@ if (process && process.argv[2] === 'html') {
 		// appElement: 'div#app',
 		// transitionElement: 'div#transition',
 		endpoints: [
-			[ (ctx) => (['GET', 'POST'].includes(ctx.request.method) && ctx.request.url.toLowerCase().startsWith('/main/')), (ctx) => {
+			[ (ctx) => (['GET', 'POST'].includes(ctx.request.method) && ctx.urlSections[0] === 'main'), (ctx) => {
 				if (ctx.request.method === 'POST') {
 					if (ctx.bodyParameters?.add) {
 						ctx.setCookie(`count=${parseInt(ctx.getCookie('count') || 0) + 1}`);
@@ -32,13 +36,13 @@ if (process && process.argv[2] === 'html') {
 					${ctx.request.method === 'POST' ? `<p>POST body parameters:</p><pre>${JSON.stringify(ctx.bodyParameters)}</pre>` : ''}
 					<p>This page was rendered at ${Date()}.</p>
 					<p>These were your cookies at time of request:</p>
-					<pre>${ctx.getCookie()}</pre>
+					<pre>${ctx.getCookie() || '[None]'}</pre>
 					<form method="POST">
 						<input type="submit" name="add" value="Add 1 to cookie"/>
 						<input type="submit" name="reset" value="Reset cookies"/>
 					</form>
 				`;
-				ctx.renderPage(content);
+				ctx.renderPage(content, 'Test');
 				// return { code: 200, headers: { 'content-type': 'text/html; charset=utf-8' }, body: content }
 			} ],
 
@@ -46,4 +50,5 @@ if (process && process.argv[2] === 'html') {
 			// [ (ctx) => (ctx.request.method === 'GET'), (ctx) => ({ code: 302, headers: { location: '/main/' } }) ],
 		],
 	});
+	console.log('Running Server...');
 };
